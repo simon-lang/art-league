a474b4decee9e78904824ec6f9018bb2fd551433
Simon Lang <simon.lang@cambia.org>
2019-02-22 08:01:41 +1000

    Tidying

--- src/app.js
+++ src/app.js
@@ -12,7 +12,21 @@ const getData = () => {
     return a.concat(b)
 }
 
-const getSpec = () => {
+const getBigData = () => {
+    const a = _.range(0, 19).map(i => ({
+        year: 2000 + i,
+        value: _.random(1, 300),
+        series: 1,
+    }))
+    const b = _.range(0, 19).map(i => ({
+        year: 2000 + i,
+        value: _.random(1, 400),
+        series: 2,
+    }))
+    return a.concat(b)
+}
+
+const getSpec = (values, width = 130, height = 40) => {
     return {
         "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
         "description": "Google's stock price over time.",
@@ -24,10 +38,10 @@ const getSpec = () => {
             }
         },
         "data": {
-            "values": getData()
+            values
         },
-        "width": 130,
-        "height": 40,
+        width,
+        height,
         "mark": "line",
         "encoding": {
             "x": {
@@ -50,10 +64,61 @@ const getSpec = () => {
     }
 }
 
-const Home = { template: require('./home.html') }
+const Home = {
+    template: require('./home.html'),
+    data: () => {
+        return {
+            artists: [
+                ['Anri Sala', 45974, 389226, '05/04/2018'],
+                ['Helen Marten', 46504, 438464, '06/04/2018'],
+                ['Ryan Gander', 47939, 377680, '07/04/2018'],
+                ['Andrea Zittel', 51718, 411401, '08/04/2018'],
+                ['Pierre Huyghe', 47837, 374527, '09/04/2018'],
+                ['Annette Messager', 50451, 397528, '10/04/2018'],
+                ['Tobor Robak', 47797, 434827, '11/04/2018'],
+                ['Ai Weiwei', 51006, 384454, '12/04/2018'],
+            ]
+        }
+    }
+}
 const Portfolio = { template: require('./portfolio.html') }
 const Profile = { template: require('./profile.html') }
-const Calculator = { template: require('./calculator.html') }
+const Calculator = {
+    template: require('./calculator.html'),
+    data: () => {
+        return {
+            value: ''
+        }
+    },
+    methods: {
+        add: function(n) {
+            this.value += n
+        }
+    }
+}
+
+window.Vue.directive('vis', {
+    inserted: function(el) {
+        const spec = getSpec(getData())
+        const opts = {
+            actions: false,
+            defaultStyle: true,
+        }
+        vegaEmbed(el, spec, opts).catch(console.error)
+    }
+})
+
+window.Vue.directive('bigVis', {
+    inserted: function(el) {
+        const spec = getSpec(getBigData(), 500, 200)
+        const opts = {
+            actions: false,
+            defaultStyle: true,
+        }
+        vegaEmbed(el, spec, opts).catch(console.error)
+    }
+})
+
 
 const routes = [
     { path: '/', component: Home },
@@ -71,15 +136,6 @@ const app = new window.Vue({
     },
     mounted: function () {
         console.log('mounted')
-        const opts = {
-            actions: false,
-            defaultStyle: true,
-        }
-        document.querySelectorAll('.vis').forEach(el => {
-            const spec = getSpec()
-            vegaEmbed(el, spec, opts).catch(console.error)
-        })
-
         // window.addEventListener('scroll', scrollWindow)
     },
     methods: {
